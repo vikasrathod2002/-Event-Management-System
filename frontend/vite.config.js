fd299e2a2df3b18395c73bba214e1c8cc70a8f83
vikasrathod2002 <vikassrathod2002@gmail.com>
2025-11-15 15:13:59 +0530

    Commit Change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://event-management-system2.onrender.com',
+        target: 'https://event-management-system8.onrender.com',
         changeOrigin: true
       }
     }
